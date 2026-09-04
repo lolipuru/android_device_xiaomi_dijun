f978b2b1758f6d0b652a33b1f9f9b59ab9d1c993
lolipuru <banstellazz@gmail.com>
2026-09-04 16:21:08 +0000

    dijun: don't included firmware
    xloader is very picky on firmware, user should better update firmware themselves for this case
    
    Change-Id: I09d7caacbff73352676fd9f7d93845e5fbc2db78
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -46,7 +46,6 @@ module = ExtractUtilsModule(
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
     check_elf=True,
-    add_firmware_proprietary_file=False,
 )
 
 if __name__ == '__main__':
