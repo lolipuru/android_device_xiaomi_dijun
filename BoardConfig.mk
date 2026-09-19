927a89ca703cf15f8d9607994f567cc2b14c7040
lolipuru <banstellazz@gmail.com>
2026-09-19 16:01:59 +0000

    dijun: refactor prebuilt kernel loading
    Change-Id: Ieda74649c0b1d248d67c79c804c1b34732ac0dc6
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,25 +13,32 @@ include device/xiaomi/O1_asic-common/BoardConfigCommon.mk
 # Display
 TARGET_SCREEN_DENSITY := 520
 
-# Dtb/o
-BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbo.img
-BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtb
-
+# Kernel
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
+BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/images/dtbs/
+BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/images/dtbo.img
 PRODUCT_COPY_FILES += \
-	$(KERNEL_PATH)/kernel:kernel
+	$(KERNEL_PATH)/images/kernel:kernel
 
-# Kernel modules
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_ramdisk/modules.load))
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_ramdisk/modules.load.recovery))
-BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_dlkm/modules.load))
+GKI_VERSION := 6.18.21-android17-5-g1d099fcb35e0-abogki538445360-4k
+DLKM_MODULES_PATH := $(KERNEL_PATH)/modules/vendor_dlkm
+RAMDISK_MODULES_PATH := $(KERNEL_PATH)/modules/vendor_boot
+SYSTEM_DLKM_MODULES_PATH := $(KERNEL_PATH)/modules/system_dlkm/$(GKI_VERSION)
+SYSTEM_DLKM_FLATTEN_MODULES_PATH := $(KERNEL_PATH)/modules/system_dlkm/flatten/lib/modules
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(KERNEL_PATH)/vendor_dlkm/,$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules) \
-    $(call find-copy-subdir-files,*,$(KERNEL_PATH)/vendor_ramdisk/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules) \
-    $(call find-copy-subdir-files,*,$(KERNEL_PATH)/system_dlkm_flatten/,$(TARGET_COPY_OUT_SYSTEM_DLKM)/flatten/lib/modules) \
-    $(call find-copy-subdir-files,*,$(KERNEL_PATH)/system_dlkm/,$(TARGET_COPY_OUT_SYSTEM_DLKM)/lib/modules/6.6.30-android15-8-g8eff17a54aa9-abogki407711482-4k)
+    $(call find-copy-subdir-files,*,$(SYSTEM_DLKM_MODULES_PATH)/,$(TARGET_COPY_OUT_SYSTEM_DLKM)/lib/modules/$(GKI_VERSION)/) \
+    $(call find-copy-subdir-files,*,$(SYSTEM_DLKM_FLATTEN_MODULES_PATH)/,$(TARGET_COPY_OUT_SYSTEM_DLKM)/flatten/lib/modules/)
+
+BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(DLKM_MODULES_PATH)/*.ko)
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(patsubst %,$(DLKM_MODULES_PATH)/%,$(shell cat $(DLKM_MODULES_PATH)/modules.load))
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DLKM_MODULES_PATH)/modules.blocklist
+
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(wildcard $(RAMDISK_MODULES_PATH)/*.ko)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(patsubst %,$(RAMDISK_MODULES_PATH)/%,$(shell cat $(RAMDISK_MODULES_PATH)/modules.load))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD  := $(patsubst %,$(RAMDISK_MODULES_PATH)/%,$(shell cat $(RAMDISK_MODULES_PATH)/modules.load.recovery))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(RAMDISK_MODULES_PATH)/modules.blocklist
 
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
